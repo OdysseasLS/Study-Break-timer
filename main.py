4ef6324834e22b2b945bd4cb8ef93ab90ef4d6e2
OdysseasLS <odysseaslyd@gmail.com>
2023-12-20 16:43:38 +0100

    made a notif for when program is started, times are printed in terminal now

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ def main():
     study_time, break_time = get_input()
     study_time *= 60
     break_time *= 60
-    take_break, back_to_work = make_notifs(study_time, break_time)
+    starting, take_break, back_to_work = make_notifs(study_time, break_time)
     run = True
     print("Started successfully! Happy studying :D!")
+    starting.send(block=False)
+    print(f"{time.ctime()}")
     while run:
         time.sleep(study_time)
         take_break.send(block=False)
-        print("break msg sent")
+        print(f"break msg sent at {time.ctime()}")
         time.sleep(break_time)
         back_to_work.send(block=False)
-        print("work msg sent")
+        print(f"work msg sent at {time.ctime()}")
 
 
 def make_notifs(study_time, break_time):
+
+    # Notification when the program starts
+    starting = Notify()
+    starting.title = "Study Timer Deployed!"
+    starting.message = f"I've been deployed at {time.ctime()}. I'll notify you when it's time to take a break."
+    starting.icon = "letsgo.jpeg"
+    starting.audio = "sound.wav"
+
     # Notification to take a break
     take_break = Notify()
     take_break.title = "Time for a break!"
@@ -33,7 +43,7 @@ def make_notifs(study_time, break_time):
     back_to_work.icon = "btw_icon.jpg"
     back_to_work.audio = "sound.wav"
 
-    return take_break, back_to_work
+    return starting, take_break, back_to_work
 
 
 def get_input():
